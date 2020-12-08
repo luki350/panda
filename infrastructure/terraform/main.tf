b289632b921189eb783a94688f6af6ec063195a5
luki350 <luki350@gmail.com>
2020-12-08 18:57:47 +0000

    infra

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,14 +7,14 @@ resource "aws_instance" "panda" {
   ami                       = "ami-07ebfd5b3428b6f4d"
   instance_type             = "t2.micro"
   availability_zone         = var.ec2_availability_zone
-  key_name                  = var.aws_key_name
+  key_name                  = "panda_kurs"
   vpc_security_group_ids    = ["${var.security_group}"]
 
   connection {
     host        = self.public_ip
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file(var.ssh_key_path)
+    private_key = "../panda_kurs.pem"
   }
 
 
